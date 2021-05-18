6826b5c9e7625d440e7cd13b9f86a79605f44b06
oisin farrell <48165055+oisinfarrell00@users.noreply.github.com>
2021-05-18 18:41:57 +0200

    under construction

--- scripts.js
+++ scripts.js
@@ -2,11 +2,14 @@ window.onscroll = function() {headerScroll()};
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+console.log("im here");
 
 function headerScroll() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    console.log("im here 2");
   } else {
     header.classList.remove("sticky");
+    console.log("im here 1");
   }
 }
